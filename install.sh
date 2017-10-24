1c1e274dcffb13fa95e983979e978b05c955b247
Kevin Daudt <me@ikke.info>
2017-10-24 23:30:58 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DESTDIR=${DESTDIR-/usr/share/zabbix/externalscripts}
+
+scripts="check_crt.py check_http check_rsync"
+libs="jq.py"
+
+for script in $scripts; do
+    install -Dm0755 $script "$DESTDIR"/$script
+done
+
+for lib in $libs; do
+    install -D lib/$lib "$DESTDIR/lib/$lib"
+done
